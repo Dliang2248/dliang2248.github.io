b7a04524a927694310fcfcf84061bc9b7eae7ab9
Dliang2248 <74386732+Dliang2248@users.noreply.github.com>
2021-01-12 15:57:50 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 // Dennis Liang
 // Last edited 1/9/21
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+console.log("variables");
+const selectDeviceBtn = document.querySelector("#select_device");
+const output = document.querySelector("#output");
+const cutDeviceBtn = document.querySelector("#cut_device");
+const output2 = document.querySelector("#output2");
+let gdxDevice;
+
 var firebaseConfig = {
   apiKey: "AIzaSyDREc2lp_WyDu7ntQ6M3UnxUzf_27Hf0bI",
   authDomain: "weatherdashboard-3bdc4.firebaseapp.com",
@@ -15,28 +21,67 @@ var firebaseConfig = {
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-// firebase.analytics();
+var sendData = null; 
 
-setInterval(function(){
-  getWeatherData();
-}, 5000);
 
-function getWeatherData(){  
-  var dbRef =  firebase.database().ref();
-  const weatherRef = dbRef.child("table"); 
-  let dataLast = " temperature is loading..."; 
-  weatherRef.on("value", snapshot => {
-    snapshot.forEach(rowSnapshot => {
-     let key = rowSnapshot.key;
-     let value = rowSnapshot.val();
-    dataLast = value.temp; 
-     console.log(value);
-     console.log(dataLast); 
+const selectDevice = async () => {
+  console.log("selectDevice function");
+  try {
+    gdxDevice = await godirect.selectDevice();
+    // print name and serial number
+    var stat = `\n Connected to ` + gdxDevice.name;
+    output.textContent = stat;
+
+    sendData = setInterval(async function(){
+  
+      var temp = parseFloat(document.getElementById("data").innerHTML);
+      if (isNaN(temp)){
+        return; 
+      }
+      console.log("Data was sent");
+      var dbRef =  firebase.database().ref();
+      const weatherRef = dbRef.child("table"); 
+      weatherRef.remove();
+      weatherRef.push({"temp":temp}); 
+  }, 500);
+  
+
+    //turns on the Default sensor
+    gdxDevice.enableDefaultSensors();
+    //create a constant enabledSensors that correlates with the enabled sensors of the gdx Device
+    const enabledSensors = gdxDevice.sensors.filter(s => s.enabled);
+
+    //create a function that runs for each of the enabled sensor measurements
+    enabledSensors.forEach(sensor => {
+      //trigger a set of actions to occur whenever the value the sensor detects changes
+      sensor.on("value-changed", sensor => {
+        document.getElementById("data").innerHTML = `\n ${sensor.value.toFixed(
+          2
+        )} ${sensor.unit}`;
+        console.log(`\n ${sensor.value.toFixed(2)} ${sensor.unit}`);
+        sensor.on("value-changed", sensor => {
+          console.log("sensor on");
+        });
+      });
     });
-  });
+  
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-  document.querySelector("#output").innerHTML = dataLast;
-  document.querySelector("#time").innerHTML = "This is the tempature at :" + new Date();
-}
+const cutDevice = async () => {
+  console.log("cutDevice function");
+  try {
+    gdxDevice.close();
+    document.getElementById("data").innerHTML = "No Data";
+    clearInterval(sendData); 
+    output.textContent = `\n Disconnected from ` + gdxDevice.name;
+  } catch (err) {
+    console.error(err);
+  }
+  
+};
 
-getWeatherData();
+selectDeviceBtn.addEventListener("click", selectDevice); //opens selection window displaying available Go Direct sensors
+cutDeviceBtn.addEventListener("click", cutDevice); //disconnects sensor device
